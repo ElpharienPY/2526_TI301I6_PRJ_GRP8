b52533336183d7ab053dadb2a8d35808c8af53e9
Adrian <adrian.rubem@efrei.net>
2025-11-19 09:36:21 +0100

    Data structure for PART2

--- include/partition.h
+++ include/partition.h
@@ -1,6 +1,35 @@
 
-
 #ifndef INC_2526_TI301I6_PRJ_GRP8_PARTITION_H
 #define INC_2526_TI301I6_PRJ_GRP8_PARTITION_H
+
+#include "tarjan.h"
+
+// Data Structures
+
+typedef struct {
+
+    int *vertices;
+
+    int size;
+
+} Class;
+
+typedef struct {
+
+    Class *classes;
+
+    int count;
+
+    int *v2c;
+
+} Partition;
+
+// Partition management
+
+Partition *partitionCreate(int vertexCount);
+
+void partitionFree(Partition *p);
+
+int partition(Partition *p,int count, const int *vertices);
 
 #endif //INC_2526_TI301I6_PRJ_GRP8_PARTITION_H
--- include/tarjan.h
+++ include/tarjan.h
@@ -1,5 +1,72 @@
 
 #ifndef INC_2526_TI301I6_PRJ_GRP8_TARJAN_H
 #define INC_2526_TI301I6_PRJ_GRP8_TARJAN_H
+ 
+#include  "adj_list.h"
 
+#include  "partition.h"
+ 
+typedef struct {
+
+    int id;
+
+    int index;
+
+    int lowlink;
+
+    int on_stack;
+
+} TarjanVertex;
+ 
+typedef struct {
+
+    int *data;
+
+    int top;
+
+    int size;
+
+} IntStack;
+ 
+// Global metadata used by Tarjan's algorithm.
+ 
+typedef struct {
+
+    TarjanVertex *vertices;
+
+    int vertex_count;
+
+    int current_index;
+
+    IntStack *stack;
+
+} TarjanMeta;
+ 
+// ========== Stack Functions ==========
+ 
+int stackInit(IntStack *stack, int size);
+ 
+int stackPush(IntStack *stack, int value);
+ 
+int stackPop(IntStack *stack);
+ 
+int stackFree(IntStack *stack);
+ 
+int stackIsEmpty(IntStack *stack);
+ 
+int stackTop(IntStack *stack);
+ 
+// ========== Tarjan Functions ==========
+ 
+TarjanMeta TarjanMetaCreate();
+ 
+void tarjanMetaFree(TarjanMeta *tarjan_meta);
+ 
+void tarjanRun(const AdjList *adj, Partition *partition);
+ 
+ 
+ 
+ 
 #endif //INC_2526_TI301I6_PRJ_GRP8_TARJAN_H
+
+ 
